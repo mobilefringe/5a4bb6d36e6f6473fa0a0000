1bb7409d260aed157fe707c68d263d50f4348e9b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 15:52:52 -0500

    Mapplic on Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -82,6 +82,9 @@
                 floor2Stores() {
                     return _.filter(this.allStores, ['z_coordinate', 2]);
                 },
+                processedCategories() {
+                    
+                },
                 getStoreSource () {
                     var mall_json = {};
                     var landmarks = {};
