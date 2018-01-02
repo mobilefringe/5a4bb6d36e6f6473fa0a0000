c8a2db1c2b7262b6260bdfd41ee3ac7e4992066c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 14:04:11 -0500

    Mapplic on Northpark 1.0  /mapplic.vue

--- mapplic.vue
+++ mapplic.vue
@@ -177,7 +177,7 @@
             methods: {
                 getTooltip () {
                     //   { thumb: true, desc: true, link: true }  
-                    return [0,10];
+                    return  { thumb: true, desc: true, link: true };
                 },
             },
             mounted() {
