7889697229db61a0cd7c0ac02f05e1c7cf059882
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 13:04:57 -0500

    Mapplic on Northpark 1.0  /mapplic.vue

---
+++ mapplic.vue
@@ -0,0 +1,30 @@
+<style>
+    .mapplic-pin{
+        /*background-color: transparent!important;    */
+    }
+    /* INTERACTIVE ELEMENTS */
+    /* clickable elements */
+    .mapplic-clickable:not(g),
+    g.mapplic-clickable > * {
+    	opacity: 1;
+    	/*fill: #b7a6bd*/;
+    }
+    
+    /* hovered elements */
+    .mapplic-clickable:not(g):hover,
+    g.mapplic-clickable:hover > * {
+    	opacity: 1;
+    	fill: #be7bf4;
+    }
+    
+    /* active elements */
+    .mapplic-active,
+    a.mapplic-active > path,
+    g.mapplic-active > * {
+    	fill: #a366d4;
+    	opacity: 1.0 !important;
+    }
+    .mapplic-coordinates {
+        visibility: hidden;
+    }
+</style>
