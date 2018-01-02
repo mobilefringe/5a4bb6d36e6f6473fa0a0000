72e8b948c2a4ac1429e00ac2a31d362c2a876d7e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 16:50:28 -0500

    Mapplic on Northpark 1.0  /mapplic.vue

--- mapplic.vue
+++ mapplic.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="position:relative;">
+  <div style="position:relative margin: auto;width: 100%;overflow: hidden;">
     <div id="mapplic" class="mapplic" @mapready="mapready" @locationopened="locationopened" @locationclosed="locationclosed" @levelswitched="levelswitched" @positionchanged="positionchanged"></div>
 </div>
 </template>
