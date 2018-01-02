24145ed142cdacc739581b0cafc44eb2ead36208
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 15:55:35 -0500

    Mapplic on Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -103,7 +103,7 @@
                     // this.floor1Stores
                     mall_json.levels=[]; 
                     // // need to add the following for each floor we want to configure.
-                    // var floor_1 ={};
+                    var floor_1 ={};
                     // floor_1.id = "first-floor";
                     // floor_1.title = "Floor 1";
                     // floor_1.map = "//www.mallmaverick.com/system/site_images/photos/000/034/516/original/Northpark_-_Map_-_Floor_1_-modifed_for_Mapplic.svg";
