46be1890d9403d54e830102a18d978f0fa3dfe5a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 16:34:42 -0500

    Mapplic on Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <svg-map :store_source="getStoreSource"></svg-map>
+        <mapplic-map :store_source="getStoreSource"></mapplic-map>
         <!--<div class="row">-->
         <!--    <div class="col-md-12">-->
         <!--        <ul class="">-->
