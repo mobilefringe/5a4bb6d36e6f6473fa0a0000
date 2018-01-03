c630a30e020abdcb0025af303baccd99618df5c5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-03 10:57:20 -0500

    Mapplic on Northpark 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -102,6 +102,45 @@
                         // }
                     });
                     mall_json.levels.push(floor_1);
+                    
+                    // need to add the following for each floor we want to configure.
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Floor 1";
+                    floor_1.map = "//www.mallmaverick.com/system/site_images/photos/000/035/861/original/NorthPark_-_Dec-15-2017_-_Floor_1.svg";
+                    //"//www.mallmaverick.com/system/site_images/photos/000/034/516/original/Northpark_-_Map_-_Floor_1_-modifed_for_Mapplic.svg";
+                    floor_1.minimap = "//codecloud.cdn.speedyrails.net/sites/5a4bb6d36e6f6473fa0a0000/image/png/1513365138000/NorthPark - Dec-15-2017 - Floor 1.png";
+                    floor_1.show = "true";
+                    floor_1.locations = [];
+                    _.forEach(this.floor1Stores, function(val, key) {
+                        //for testing limiting the store numbers to this
+                        // if(key < 10) {
+                        var temp_val = {};
+                        temp_val.id = val.svgmap_region;
+                        temp_val.title = val.name;
+                        temp_val.about = $.trim(val.description).substring(0, 200).split(" ").slice(0, -1).join(" ") + "...";;
+                        temp_val.category = val.categories[1];
+                        // temp_val.thumbnail = "";
+                        temp_val.link = "/stores/" + val.slug;
+                        temp_val.pin = "hidden";
+                        // temp_val.action = "none";
+                        temp_val.fill = "#";
+                        // if(val.store_front_url_abs.indexOf("missing") == -1){
+                        //     temp_val.thumbnail=val.store_front_url_abs;
+                        // }
+                        //get svg's wifth/height by checking the map
+                        var svg_width = 1530;
+                        var svg_height = 1358;
+
+                        temp_val.x = val.x_coordinate / svg_width;
+                        temp_val.y = val.y_coordinate / svg_height;
+                        floor_1.locations.push(temp_val);
+                        // }
+                        // else {
+                        //     return;
+                        // }
+                    });
+                    mall_json.levels.push(floor_1);
                     return mall_json;
                 }
             }
